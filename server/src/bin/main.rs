e613e001485aa25f02028910a16b371d1157dd76
William J <william@mobilecoin.com>
2023-01-25 15:19:29 -0500

    Remove unwanted file and add space between logical blocks

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -21,6 +21,7 @@ async fn main() {
 
     let (msg_bus_tx, mut msg_bus_rx) = broadcast::channel::<Msg>(MSG_BUS_QUEUE_SIZE);
     let quote_book = InMemoryQuoteBook::default();
+
     // Open the ledger db
     let ledger_db = LedgerDB::open(&config.ledger_db).expect("Could not open ledger db");
     let num_blocks = ledger_db
