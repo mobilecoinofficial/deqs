ba39d79db42ec07462c3ebc36668a4589345aa47
Eran Rundstein <eran@rundste.in>
2023-01-25 11:47:13 -0800

    gossip with production(?) transport

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -4,22 +4,32 @@ use clap::Parser;
 use deqs_quote_book::InMemoryQuoteBook;
 use deqs_server::{Msg, Server, ServerConfig};
 use libp2p::{
+    core::upgrade,
     futures::StreamExt,
-    identity,
-    identify,
+    gossipsub::{
+        Gossipsub, GossipsubEvent, GossipsubMessage, IdentTopic as Topic, MessageAuthenticity,
+        MessageId, ValidationMode,
+    },
+    identify, identity,
     kad::{
         record::{store::MemoryStore, Key},
         Kademlia, KademliaConfig, KademliaEvent, QueryResult,
     },
-    mdns, ping,
+    mdns, mplex, noise, ping,
     swarm::{SwarmBuilder, SwarmEvent},
-    tokio_development_transport, Multiaddr, NetworkBehaviour, PeerId,
+    tcp::{GenTcpConfig, TokioTcpTransport},
+    Multiaddr, NetworkBehaviour, PeerId, Transport,
 };
 use libp2p_swarm::{keep_alive, NetworkBehaviour};
 use mc_common::logger::{log, o};
 use mc_util_grpc::AdminServer;
 use postage::{broadcast, prelude::Stream};
-use std::{str::FromStr, sync::Arc};
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+    str::FromStr,
+    sync::Arc,
+};
 use tokio::{io::AsyncBufReadExt, time::Duration};
 use void::Void;
 
@@ -33,6 +43,7 @@ struct Behaviour {
     ping: ping::Behaviour,
     mdns: mdns::TokioMdns,
     kademlia: Kademlia<MemoryStore>,
+    gossipsub: Gossipsub,
 }
 
 #[allow(clippy::large_enum_variant)]
@@ -43,6 +54,7 @@ enum OutEvent {
     Void(Void),
     Kademlia(KademliaEvent),
     Identify(identify::Event),
+    Gossipsub(GossipsubEvent),
 }
 impl From<mdns::MdnsEvent> for OutEvent {
     fn from(event: mdns::MdnsEvent) -> Self {
@@ -69,6 +81,11 @@ impl From<identify::Event> for OutEvent {
         OutEvent::Identify(event)
     }
 }
+impl From<GossipsubEvent> for OutEvent {
+    fn from(event: GossipsubEvent) -> Self {
+        OutEvent::Gossipsub(event)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -85,15 +102,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
     log::info!(logger, "p2p: local peer id: {:?}", local_peer_id);
-    //let transport = libp2p::development_transport(local_key).await?;
-    // let transport = TokioTcpTransport::new(GenTcpConfig::default().nodelay(true))
-    //     .upgrade(upgrade::Version::V1)
-    //     .authenticate(
-    //         noise::NoiseAuthenticated::xx(&local_key)
-    //             .expect("Signing libp2p-noise static DH keypair failed."),
-    //     )
-    //     .multiplex(mplex::MplexConfig::new())
-    //     .boxed();
+
+    let topic = Topic::new("test-net");
+    // To content-address message, we can take the hash of message and use it as an
+    // ID.
+    let message_id_fn = |message: &GossipsubMessage| {
+        let mut s = DefaultHasher::new();
+        message.data.hash(&mut s);
+        MessageId::from(s.finish().to_string())
+    };
+
+    // Set a custom gossipsub
+    let gossipsub_config = libp2p::gossipsub::GossipsubConfigBuilder::default()
+        .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+        .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+        .message_id_fn(message_id_fn) // content-address messages. No two messages of the
+        // same content will be propagated.
+        .build()
+        .expect("Valid config");
+    // build a gossipsub network behaviour
+    let mut gossipsub: Gossipsub = Gossipsub::new(
+        MessageAuthenticity::Signed(local_key.clone()),
+        gossipsub_config,
+    )
+    .expect("Correct configuration");
 
     // Create a Kademlia behaviour.
     let mut cfg = KademliaConfig::default();
@@ -104,6 +136,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let remote: Multiaddr = peer.parse()?;
         //swarm.dial(remote)?;
         kademlia.add_address(&PeerId::from_str(peer_id)?, remote);
+        gossipsub.add_explicit_peer(&PeerId::from_str(peer_id)?);
         log::info!(&logger, "p2p: Dialed {}", peer);
     }
     let key = Key::new(&"keh");
@@ -111,30 +144,61 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .start_providing(key.clone())
         .expect("Failed to start providing key");
 
+    // subscribes to our topic
+    gossipsub.subscribe(&topic).unwrap();
+
     let behaviour = Behaviour {
         keep_alive: keep_alive::Behaviour::default(),
         ping: ping::Behaviour::default(),
         mdns: mdns::TokioMdns::new(mdns::MdnsConfig::default())?,
         kademlia,
+        gossipsub,
     };
-    let mut swarm = SwarmBuilder::new(
-        tokio_development_transport(local_key)?,
-        behaviour,
-        local_peer_id,
-    )
-    .executor(Box::new(|fut| {
-        tokio::spawn(fut);
-    }))
-    .build();
+
+    let transport = TokioTcpTransport::new(GenTcpConfig::default().nodelay(true))
+        .upgrade(upgrade::Version::V1)
+        .authenticate(
+            noise::NoiseAuthenticated::xx(&local_key)
+                .expect("Signing libp2p-noise static DH keypair failed."),
+        )
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+
+    //let transport = tokio_development_transport(local_key)?;
+
+    let mut swarm = SwarmBuilder::new(transport, behaviour, local_peer_id)
+        .executor(Box::new(|fut| {
+            tokio::spawn(fut);
+        }))
+        .build();
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
     let logger2 = logger.clone();
     tokio::spawn(async move {
         println!("in swarm loop");
+        let mut x = 0;
         loop {
             tokio::select! {
                 line = stdin.next_line() => {
-                println!("reading done: {:?}", line);
-                        swarm.behaviour_mut().kademlia.get_providers(key.clone());
+                    println!("reading done: {:?}", line);
+                    let line = line.unwrap().unwrap().trim().to_string();
+                    match line.as_str() {
+                        "prov" => {
+                            swarm.behaviour_mut().kademlia.get_providers(key.clone());
+                        }
+                        "gos" => {
+                            if let Err(e) = swarm
+                                .behaviour_mut()
+                                .gossipsub
+                                .publish(topic.clone(), x.to_string().as_bytes())
+                            {
+                                println!("Publish error: {:?}", e);
+                            }
+                            x += 1;
+                        }
+                        l => {
+                            println!("???? {:?}", l);
+                        }
+                    }
                 }
                 sel = swarm.select_next_some() => match sel {
                     SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {:?}", address),
@@ -175,7 +239,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                     SwarmEvent::Behaviour(OutEvent::Identify(e)) => {
                         log::info!(&logger2, "IdentifyEvent: {:?}", e);
-    
+
                         if let identify::Event::Received {
                             peer_id,
                             info:
