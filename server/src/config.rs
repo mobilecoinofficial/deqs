5c2a357a541569090691bff6859d6facbbdabcfc
wjuan-mob <william@mobilecoin.com>
2023-01-26 14:21:07 -0500

    Add ledgerdb cli and open ledger (#8)
    * Add ledgerdb cli and open ledger
    
    * Add protoset to gitignore
    
    Co-authored-by: Eran Rundstein <eran@rundste.in>

--- server/src/config.rs
+++ server/src/config.rs
@@ -6,6 +6,7 @@ use clap::Parser;
 use deqs_api::DeqsClientUri;
 use mc_util_uri::AdminUri;
 use serde::Serialize;
+use std::path::PathBuf;
 
 /// Command-line configuration options for the DEQS server
 #[derive(Parser, Serialize)]
@@ -18,4 +19,8 @@ pub struct ServerConfig {
     /// Optional admin listening URI.
     #[clap(long, env = "MC_ADMIN_LISTEN_URI")]
     pub admin_listen_uri: Option<AdminUri>,
+
+    /// Path to ledgerdb
+    #[clap(long = "ledger-db", env = "MC_LEDGER_DB")]
+    pub ledger_db: PathBuf,
 }
