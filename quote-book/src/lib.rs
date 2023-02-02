afffc718273831d86e37e83957d405abbcc6ef63
William J <william@mobilecoin.com>
2023-02-02 03:14:04 -0500

    Add Synchronized Quotebook

--- quote-book/src/lib.rs
+++ quote-book/src/lib.rs
@@ -5,11 +5,14 @@
 mod basic_types;
 mod error;
 mod in_memory_quote_book;
+mod synchronized_quote_book;
 mod quote;
 mod quote_book;
 
 pub use basic_types::{Pair, QuoteId};
 pub use error::Error;
 pub use in_memory_quote_book::InMemoryQuoteBook;
+pub use synchronized_quote_book::SynchronizedQuoteBook;
+
 pub use quote::Quote;
 pub use quote_book::QuoteBook;
