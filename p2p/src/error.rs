7b335d379ad9113b5f8a165b1d4e30328f833ea8
Eran Rundstein <eran@rundste.in>
2023-02-04 12:03:01 -0800

    add p2p crate

---
+++ p2p/src/error.rs
@@ -0,0 +1,86 @@
+// Copyright (c) 2023 MobileCoin Inc.
+
+use displaydoc::Display;
+use libp2p::{
+    kad::store::Error as KadStoreError, multiaddr::Error as MultiaddrError, noise::NoiseError,
+    Multiaddr, TransportError,
+};
+use libp2p_swarm::DialError;
+use std::io::Error as IoError;
+
+/// Error data type
+#[derive(Debug, Display)]
+pub enum Error {
+    /// Gossipsub build error: {0}
+    GossipsubBuild(&'static str),
+
+    /// Gossipsub new error: {0}
+    GossipsubNew(&'static str),
+
+    /// Io error: {0}
+    Io(IoError),
+
+    /// Noise: {0}
+    Noise(NoiseError),
+
+    /// Dial: {0}
+    Dial(DialError),
+
+    /// Bootstrap: {0}
+    Bootstrap(String),
+
+    /// Transport: {0}
+    Transport(TransportError<IoError>),
+
+    /// Multihash: {0}
+    Multihash(String),
+
+    /** Invalid peer address {0}, expected last component to be p2p multihash
+     * and instead got {1}
+     */
+    InvalidPeerAddress(Multiaddr, String),
+
+    /// Multiaddr: {0}
+    Multiaddr(MultiaddrError),
+
+    /// Kademlia store: {0}
+    KadStore(KadStoreError),
+}
+
+impl From<IoError> for Error {
+    fn from(e: IoError) -> Self {
+        Self::Io(e)
+    }
+}
+
+impl From<NoiseError> for Error {
+    fn from(e: NoiseError) -> Self {
+        Self::Noise(e)
+    }
+}
+
+impl From<DialError> for Error {
+    fn from(e: DialError) -> Self {
+        Self::Dial(e)
+    }
+}
+
+impl From<TransportError<IoError>> for Error {
+    fn from(e: TransportError<IoError>) -> Self {
+        Self::Transport(e)
+    }
+}
+
+impl From<MultiaddrError> for Error {
+    fn from(e: MultiaddrError) -> Self {
+        Self::Multiaddr(e)
+    }
+}
+
+impl From<KadStoreError> for Error {
+    fn from(e: KadStoreError) -> Self {
+        Self::KadStore(e)
+    }
+}
+
+impl std::error::Error for Error {}
---
+++ p2p/src/network_builder.rs
@@ -0,0 +1,127 @@
+// Copyright (c) 2023 MobileCoin Inc.
+
+use crate::{
+    client::Client, network_event_loop::NetworkEventLoop, Behaviour, Error, Network, RpcRequest,
+    RpcResponse,
+};
+use libp2p::{
+    core::{muxing::StreamMuxerBox, transport, transport::upgrade, upgrade::SelectUpgrade},
+    dns,
+    identity::Keypair,
+    mplex, noise,
+    swarm::SwarmBuilder,
+    tcp, websocket, yamux, Multiaddr, PeerId, Transport,
+};
+use mc_common::logger::{log, Logger};
+use tokio::sync::mpsc;
+
+/// A builder object for constructing a p2p network.
+/// The output of this is a `Network`.
+pub struct NetworkBuilder<REQ: RpcRequest, RESP: RpcResponse> {
+    keypair: Keypair,
+    transport: transport::Boxed<(PeerId, StreamMuxerBox)>,
+    listen_address: Multiaddr,
+    external_addresses: Vec<Multiaddr>,
+    behaviour: Behaviour<REQ, RESP>,
+    bootstrap_peers: Vec<Multiaddr>,
+    logger: Logger,
+}
+
+impl<REQ: RpcRequest, RESP: RpcResponse> NetworkBuilder<REQ, RESP> {
+    pub fn new(
+        keypair: Keypair,
+        behaviour: Behaviour<REQ, RESP>,
+        bootstrap_peers: Vec<Multiaddr>,
+        logger: Logger,
+    ) -> Result<Self, Error> {
+        Ok(Self {
+            transport: Self::default_transport(&keypair)?,
+            keypair,
+            listen_address: Self::default_listen_address()?,
+            external_addresses: vec![],
+            behaviour,
+            bootstrap_peers,
+            logger,
+        })
+    }
+
+    pub fn listen_address(mut self, address: Multiaddr) -> Self {
+        self.listen_address = address;
+        self
+    }
+
+    pub fn external_addresses(mut self, addresses: Vec<Multiaddr>) -> Self {
+        self.external_addresses = addresses;
+        self
+    }
+
+    pub fn transport(mut self, transport: transport::Boxed<(PeerId, StreamMuxerBox)>) -> Self {
+        self.transport = transport;
+        self
+    }
+
+    /// Listen on all interfaces, on a random port
+    fn default_listen_address() -> Result<Multiaddr, Error> {
+        Ok("/ip4/127.0.0.1/tcp/0".parse()?)
+    }
+
+    // Mostly copied from development_transport in libp2p
+    fn default_transport(
+        keypair: &Keypair,
+    ) -> Result<transport::Boxed<(PeerId, StreamMuxerBox)>, Error> {
+        Ok({
+            let dns_tcp = dns::TokioDnsConfig::system(tcp::TokioTcpTransport::new(
+                tcp::GenTcpConfig::new().nodelay(true),
+            ))?;
+            let ws_dns_tcp = websocket::WsConfig::new(dns::TokioDnsConfig::system(
+                tcp::TokioTcpTransport::new(tcp::GenTcpConfig::new().nodelay(true)),
+            )?);
+            dns_tcp.or_transport(ws_dns_tcp)
+        }
+        .upgrade(upgrade::Version::V1)
+        .authenticate(noise::NoiseAuthenticated::xx(keypair)?)
+        .multiplex(SelectUpgrade::new(
+            yamux::YamuxConfig::default(),
+            mplex::MplexConfig::default(),
+        ))
+        .timeout(std::time::Duration::from_secs(20))
+        .boxed())
+    }
+
+    pub fn build(self) -> Result<Network<REQ, RESP>, Error> {
+        let mut swarm = SwarmBuilder::new(
+            self.transport,
+            self.behaviour,
+            PeerId::from(self.keypair.public()),
+        )
+        .executor(Box::new(|fut| {
+            tokio::spawn(fut);
+        }))
+        .build();
+
+        swarm.listen_on(self.listen_address)?;
+        for addr in self.external_addresses {
+            swarm.add_external_address(addr, libp2p_swarm::AddressScore::Infinite);
+        }
+
+        log::info!(&self.logger, "Local PeerId: {}", swarm.local_peer_id());
+
+        let (command_sender, command_receiver) = mpsc::unbounded_channel();
+        let (event_sender, event_receiver) = mpsc::unbounded_channel();
+
+        let client = Client::new(command_sender);
+
+        let event_loop_handle = NetworkEventLoop::start(
+            command_receiver,
+            event_sender,
+            swarm,
+            self.bootstrap_peers.clone(),
+            self.logger,
+        );
+        Ok(Network {
+            event_loop_handle,
+            client,
+            events: event_receiver,
+        })
+    }
+}
