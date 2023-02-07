ef40475c761111bd40a4ac933f7a5078ff30650a
William J <william@mobilecoin.com>
2023-02-06 23:10:10 -0500

    Use ledgerdb instead of mockledger per pr comments

--- quote-book/tests/synchronized_quote_book.rs
+++ quote-book/tests/synchronized_quote_book.rs
@@ -2,12 +2,37 @@
 
 mod common;
 
-use deqs_quote_book::{InMemoryQuoteBook, SynchronizedQuoteBook};
-use mc_ledger_db::test_utils::MockLedger;
+use deqs_quote_book::{Error, InMemoryQuoteBook, QuoteBook, SynchronizedQuoteBook};
+use mc_blockchain_types::BlockVersion;
+use mc_crypto_ring_signature_signer::NoKeysRingSigner;
+use mc_ledger_db::LedgerDB;
+use rand::{rngs::StdRng, SeedableRng};
+
+use mc_account_keys::AccountKey;
+use mc_fog_report_validation_test_utils::MockFogResolver;
+use mc_ledger_db::test_utils::{
+    add_txos_and_key_images_to_ledger, create_ledger, initialize_ledger,
+};
+use mc_transaction_builder::test_utils::get_transaction;
+use mc_transaction_core::TokenId;
+
+fn create_and_initialize_test_ledger() -> LedgerDB {
+    //Create a ledger_db
+    let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
+    let block_version = BlockVersion::MAX;
+    let sender = AccountKey::random(&mut rng);
+    let mut ledger = create_ledger();
+
+    //Initialize that db
+    let n_blocks = 3;
+    initialize_ledger(block_version, &mut ledger, n_blocks, &sender, &mut rng);
+
+    ledger
+}
 
 #[test]
 fn basic_happy_flow() {
-    let ledger = MockLedger::default();
+    let ledger = create_and_initialize_test_ledger();
     let quote_book = InMemoryQuoteBook::default();
     let synchronized_quote_book = SynchronizedQuoteBook::new(quote_book, ledger);
     common::basic_happy_flow(&synchronized_quote_book);
@@ -15,7 +40,7 @@ fn basic_happy_flow() {
 
 #[test]
 fn cannot_add_invalid_sci() {
-    let ledger = MockLedger::default();
+    let ledger = create_and_initialize_test_ledger();
     let quote_book = InMemoryQuoteBook::default();
     let synchronized_quote_book = SynchronizedQuoteBook::new(quote_book, ledger);
     common::cannot_add_invalid_sci(&synchronized_quote_book);
@@ -23,7 +48,7 @@ fn cannot_add_invalid_sci() {
 
 #[test]
 fn get_quotes_filtering_works() {
-    let ledger = MockLedger::default();
+    let ledger = create_and_initialize_test_ledger();
     let quote_book = InMemoryQuoteBook::default();
     let synchronized_quote_book = SynchronizedQuoteBook::new(quote_book, ledger);
     common::get_quotes_filtering_works(&synchronized_quote_book);
@@ -31,71 +56,51 @@ fn get_quotes_filtering_works() {
 
 #[test]
 fn cannot_add_stale_sci() {
-    let mut ledger = MockLedger::default();
-    let quote_book = InMemoryQuoteBook::default();
-    let synchronized_quote_book = SynchronizedQuoteBook::new(quote_book, ledger.clone());
-    common::add_quote_already_in_ledger_should_fail(&synchronized_quote_book, &mut ledger);
-}
+    let mut ledger = create_and_initialize_test_ledger();
+    let unwrapped_quote_book = InMemoryQuoteBook::default();
+    let synchronized_quote_book = SynchronizedQuoteBook::new(unwrapped_quote_book, ledger.clone());
 
-/// Test adding a quote which is already in the ledger
-pub fn add_quote_already_in_ledger_should_fail(
-    quote_book: &impl QuoteBook,
-    ledger: &mut impl Ledger,
-) {
-    let pair = pair();
+    let pair = common::pair();
     let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
 
-    let sci_builder = create_sci_builder(&pair, 10, 20, &mut rng);
+    let sci_builder = common::create_sci_builder(&pair, 10, 20, &mut rng);
     let sci = sci_builder.build(&NoKeysRingSigner {}, &mut rng).unwrap();
-    add_key_image_to_ledger(ledger, BlockVersion::MAX, vec![sci.key_image()], &mut rng).unwrap();
+
+    let block_version = BlockVersion::MAX;
+    let fog_resolver = MockFogResolver::default();
+
+    let offerer_account = AccountKey::random(&mut rng);
+
+    let tx = get_transaction(
+        block_version,
+        TokenId::from(0),
+        2,
+        2,
+        &offerer_account,
+        &offerer_account,
+        fog_resolver,
+        &mut rng,
+    )
+    .unwrap();
+    add_txos_and_key_images_to_ledger(
+        &mut ledger,
+        BlockVersion::MAX,
+        tx.prefix.outputs,
+        vec![sci.key_image()],
+        &mut rng,
+    )
+    .unwrap();
 
     //Because the key image is already in the ledger, adding this sci should fail
     assert_eq!(
-        quote_book.add_sci(sci, None).unwrap_err(),
+        synchronized_quote_book.add_sci(sci, None).unwrap_err(),
         Error::QuoteIsStale
     );
 
     //Adding a quote that isn't already in the ledger should work
-    let sci = create_sci(&pair, 10, 20, &mut rng);
-    let quote = quote_book.add_sci(sci, None).unwrap();
+    let sci = common::create_sci(&pair, 10, 20, &mut rng);
+    let quote = synchronized_quote_book.add_sci(sci, None).unwrap();
 
-    let quotes = quote_book.get_quotes(&pair, .., 0).unwrap();
+    let quotes = synchronized_quote_book.get_quotes(&pair, .., 0).unwrap();
     assert_eq!(quotes, vec![quote.clone()]);
-}
-
-/// Adds a block containing the given keyimage to the ledger and returns the new
-/// block.
-///
-/// # Arguments
-/// # TODO(wjuan): This should be refactored in along with the test_utils for adding to ledger in the mobilecoin repo.
-/// * `ledger` - Ledger instance.
-/// * `block_version` - The block version to use.
-/// * `key image` - The key-image to be added
-/// * `rng` - Random number generator.
-pub fn add_key_image_to_ledger(
-    ledger_db: &mut impl Ledger,
-    block_version: BlockVersion,
-    key_images: Vec<KeyImage>,
-    rng: &mut (impl CryptoRng + RngCore),
-) -> Result<BlockData, Error> {
-    let num_blocks = ledger_db.num_blocks()?;
-    let block_contents = BlockContents {
-        key_images,
-        ..Default::default()
-    };
-    let new_block = if num_blocks > 0 {
-        let parent = ledger_db.get_block(num_blocks - 1)?;
-
-        Block::new_with_parent(block_version, &parent, &Default::default(), &block_contents)
-    } else {
-        Block::new_origin_block(&block_contents.outputs)
-    };
-
-    let signature = make_block_signature(&new_block, rng);
-    let metadata = make_block_metadata(new_block.id.clone(), rng);
-    let block_data = BlockData::new(new_block, block_contents, signature, metadata);
-
-    ledger_db.append_block_data(&block_data)?;
-
-    Ok(block_data)
 }
