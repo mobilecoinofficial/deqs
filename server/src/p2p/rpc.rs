e59ba015284251a12d50fd7cf22ee2b7bfa633eb
Eran Rundstein <eran@rundste.in>
2023-02-08 12:31:41 -0800

    fetch quotes in batches

--- server/src/p2p/rpc.rs
+++ server/src/p2p/rpc.rs
@@ -1,9 +1,6 @@
 // Copyright (c) 2023 MobileCoin Inc.
 
-use crate::{
-    p2p::{RpcError, RpcQuoteBookError},
-    Error,
-};
+use crate::{p2p::RpcError, Error};
 use deqs_p2p::{libp2p::PeerId, Client};
 use deqs_quote_book::{Quote, QuoteId};
 use mc_common::logger::{log, Logger};
@@ -12,13 +9,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub enum Request {
     GetAllQuoteIds,
-    GetQuoteById(QuoteId),
+    GetQuotesById(Vec<QuoteId>),
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub enum Response {
     AllQuoteIds(Vec<QuoteId>),
-    MaybeQuote(Option<Quote>),
+    MaybeQuotes(Vec<Result<Option<Quote>, RpcError>>),
     Error(RpcError),
 }
 
@@ -69,19 +66,32 @@ impl RpcClient {
         }
     }
 
-    pub async fn get_quote_by_id(
+    pub async fn get_quotes_by_id(
         &mut self,
         peer_id: PeerId,
-        quote_id: QuoteId,
-    ) -> Result<Option<Quote>, Error> {
+        quote_ids: Vec<QuoteId>,
+    ) -> Result<Vec<Result<Option<Quote>, RpcError>>, Error> {
+        let quote_ids_len = quote_ids.len();
+
         match self
             .client
-            .rpc_request(peer_id, Request::GetQuoteById(quote_id))
+            .rpc_request(peer_id, Request::GetQuotesById(quote_ids))
             .await
         {
-            Ok(Response::MaybeQuote(quote)) => Ok(quote),
-
-            Ok(Response::Error(RpcError::QuoteBook(RpcQuoteBookError::QuoteNotFound))) => Ok(None),
+            Ok(Response::MaybeQuotes(quotes)) => {
+                if quote_ids_len == quotes.len() {
+                    Ok(quotes)
+                } else {
+                    log::warn!(
+                        self.logger,
+                        "Received unexpected number of quotes from {}: expected {}, got {}",
+                        peer_id,
+                        quote_ids_len,
+                        quotes.len()
+                    );
+                    Err(Error::P2PRpc(RpcError::UnexpectedResponse))
+                }
+            }
 
             Ok(Response::Error(err)) => {
                 log::info!(self.logger, "Received error from peer {}: {}", peer_id, err);
