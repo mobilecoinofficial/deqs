218e5e2eb1677c2c0bd3cf24a2badf7f83f9ede0
Eran Rundstein <eran@rundste.in>
2023-02-10 11:14:32 -0800

    expand two node e2e test

--- server/tests/e2e.rs
+++ server/tests/e2e.rs
@@ -2,7 +2,11 @@
 
 use std::{str::FromStr, sync::Arc, time::Duration};
 
-use deqs_api::{deqs::SubmitQuotesRequest, deqs_grpc::DeqsClientApiClient, DeqsClientUri};
+use deqs_api::{
+    deqs::{RemoveQuoteRequest, SubmitQuotesRequest},
+    deqs_grpc::DeqsClientApiClient,
+    DeqsClientUri,
+};
 use deqs_quote_book::{InMemoryQuoteBook, QuoteBook, QuoteId, SynchronizedQuoteBook};
 use deqs_server::Server;
 use grpcio::{ChannelBuilder, EnvBuilder};
@@ -32,7 +36,7 @@ fn create_and_initialize_test_ledger() -> LedgerDB {
 }
 
 #[async_test_with_logger]
-async fn e2e(logger: Logger) {
+async fn two_nodes_e2e(logger: Logger) {
     let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
     let ledger_db = create_and_initialize_test_ledger();
 
@@ -41,7 +45,7 @@ async fn e2e(logger: Logger) {
     let synchronized_quote_book1 =
         SynchronizedQuoteBook::new(internal_quote_book1, ledger_db.clone());
     let deqs_server1 = Server::start(
-        synchronized_quote_book1,
+        synchronized_quote_book1.clone(),
         DeqsClientUri::from_str("insecure-deqs://127.0.0.1:0/").unwrap(),
         vec![],
         None,
@@ -55,7 +59,7 @@ async fn e2e(logger: Logger) {
     let internal_quote_book2 = InMemoryQuoteBook::default();
     let synchronized_quote_book2 =
         SynchronizedQuoteBook::new(internal_quote_book2, ledger_db.clone());
-    let _deqs_server2 = Server::start(
+    let deqs_server2 = Server::start(
         synchronized_quote_book2.clone(),
         DeqsClientUri::from_str("insecure-deqs://127.0.0.1:0/").unwrap(),
         deqs_server1.p2p_listen_addrs(),
@@ -71,16 +75,18 @@ async fn e2e(logger: Logger) {
     let sci =
         deqs_mc_test_utils::create_sci(TokenId::from(1), TokenId::from(2), 10000, 20000, &mut rng);
 
-    let client_env = Arc::new(EnvBuilder::new().build());
-    let ch = ChannelBuilder::default_channel_builder(client_env)
+    let client1_env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::default_channel_builder(client1_env)
         .connect_to_uri(&deqs_server1.grpc_listen_uri().unwrap(), &logger);
-    let client_api = DeqsClientApiClient::new(ch);
+    let client1_api = DeqsClientApiClient::new(ch);
 
     let req = SubmitQuotesRequest {
         quotes: vec![(&sci).into()].into(),
         ..Default::default()
     };
-    let resp = client_api.submit_quotes(&req).expect("submit quote failed");
+    let resp = client1_api
+        .submit_quotes(&req)
+        .expect("submit quote failed");
     let quote_id = QuoteId::try_from(resp.get_quotes()[0].get_id()).unwrap();
 
     // After a few moments the second server should have the SCI in its quote book
@@ -97,4 +103,37 @@ async fn e2e(logger: Logger) {
     .unwrap();
 
     assert_eq!(quote.sci(), &sci);
+
+    // Request the second server to remove the SCI, the change should propagate to
+    // the first server. First sanity test that SCI is in fact in the quote book
+    // of the first server.
+    assert_eq!(
+        synchronized_quote_book1
+            .get_quote_by_id(&quote_id)
+            .unwrap()
+            .unwrap(),
+        quote
+    );
+
+    let client2_env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::default_channel_builder(client2_env)
+        .connect_to_uri(&deqs_server2.grpc_listen_uri().unwrap(), &logger);
+    let client2_api = DeqsClientApiClient::new(ch);
+
+    let mut req = RemoveQuoteRequest::default();
+    req.set_quote_id((&quote_id).into());
+    let _resp = client2_api.remove_quote(&req).expect("remove quote failed");
+
+    // Quote should eventually be removed from the first server
+    let retry_strategy = FixedInterval::new(Duration::from_secs(1)).take(10); // limit to 10 retries
+    Retry::spawn(retry_strategy, || async {
+        let quote = synchronized_quote_book1.get_quote_by_id(&quote_id);
+        match quote {
+            Ok(Some(_quote)) => Err("not yet".to_string()),
+            Ok(None) => Ok(()),
+            Err(e) => Err(format!("error: {:?}", e)),
+        }
+    })
+    .await
+    .unwrap();
 }
